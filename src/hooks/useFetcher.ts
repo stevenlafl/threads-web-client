145b5006591ee9172f86b9777cea18491d4fe883
Steven Linn <smlucf@gmail.com>
2023-07-16 06:07:32 -0600

    Ensure no queries besides login hit Threads if there is no token present

--- src/hooks/useFetcher.ts
+++ src/hooks/useFetcher.ts
@@ -1,16 +1,20 @@
 // @see: https://medium.com/@oherterich/creating-a-textarea-with-dynamic-height-using-react-and-typescript-5ed2d78d9848
 
-import { selectToken, selectUserId } from "@/store/authSlice";
+import { selectAuthState, selectToken, selectUserId } from "@/store/authSlice";
 import { useSelector } from "react-redux";
 
 // Updates the height of a <textarea> when the value changes.
 const useFetcher = () => {
 
+  const isLoggedIn = useSelector(selectAuthState);
   const token = useSelector(selectToken);
   const my_user_id = useSelector(selectUserId);
 
   return async (endpoint: string, data: Object = {}) => {
     
+    if (!isLoggedIn || !token || !my_user_id)  {
+      return false;
+    }
     let response = await fetch(endpoint, {
       method: 'POST',
       body: JSON.stringify({
